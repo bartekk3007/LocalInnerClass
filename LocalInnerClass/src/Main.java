78ba8e2ad45a3831b36e5d6d53215253b31addb2
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-09-29 23:51:54 +0200

    Add files via upload

---
+++ LocalInnerClass/src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Person p1 = new Person("Bartek", "Kołakowski", 30);
+        // local inner class fields are unavailable outside of a method where it was declared
+
+    }
+}
